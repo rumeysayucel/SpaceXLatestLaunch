52daa543b51d8aa23014e3ae67f00bdf0e7c0540
Rumeysa Yücel <rumeysa.yucel.1996@gmail.com>
2022-02-04 19:37:28 +0300

    Network class created.

--- SpaceXLatestLaunch/Network/Network.swift
+++ SpaceXLatestLaunch/Network/Network.swift
@@ -7,4 +7,32 @@
 
 import UIKit
 
+class Network {
+    func fetchLaunchInfo(completion: @escaping (LaunchInfo?) -> Void) {
+        let baseUrl = URL(string: "https://api.spacexdata.com/v4/launches/latest")!
 
+        let dataTask = URLSession.shared.dataTask(with: baseUrl) { (data, _, error) in
+            let jsonDecoder = JSONDecoder()
+            
+            if let data = data, let photoInfoObject = try? jsonDecoder.decode(LaunchInfo.self, from: data) {
+                completion(photoInfoObject)
+            }else {
+                completion(nil)
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+    func fetchLaunchPhoto(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+            if let data = data, let image = UIImage(data: data) {
+                completion(image)
+            }else {
+                completion(nil)
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
