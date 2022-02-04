3ee806d86eeaabc4723ef2ab5a79e5692bfcbd51
Rumeysa Yücel <rumeysa.yucel.1996@gmail.com>
2022-02-04 19:26:26 +0300

    UI Design Completed

---
+++ SpaceXLatestLaunch/LaunchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SpaceXLatestLaunch
+//
+//  Created by Rumeysa Yücel on 4.02.2022.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var rocketLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
